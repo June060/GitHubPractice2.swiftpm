cad1aab30c88b0e6038ca524d72ab889cb9c4dc8
Nicholas Scott <nscott4525@stu.d214.org>
2023-04-06 14:33:57 -0500

    added binding var to overview

--- VerbView.swift
+++ VerbView.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 
 struct VerbView: View {
+    @Binding var madLib:Madlib
     var body: some View {
         NavigationView {
+            TextField("Enter a verb", text: $madLib.verb)
             NavigationLink("Adjective View") {
-                AdjectiveView()
+                AdjectiveView(madLib)
             }
         }
     }
